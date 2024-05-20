2ecb9ce63d8b5a76faf1cde753c094866cee10d0
Ana Paula Sá <114103289+anapsa@users.noreply.github.com>
2024-05-20 20:15:22 -0300

    Create F.hs

---
+++ Lista 2/F.hs
@@ -0,0 +1,12 @@
+--altura da árvore
+data Tree t = Node t (Tree t) (Tree t) 
+              | Nilt
+              deriving (Read)
+              
+alturaArvore :: Tree t -> Int
+alturaArvore Nilt = 0 
+alturaArvore (Node node right left) = 1 + max (alturaArvore(right)) (alturaArvore(left))
+main = do
+       a <- getLine
+       let result = alturaArvore (read a::Tree Int)
+       print result
