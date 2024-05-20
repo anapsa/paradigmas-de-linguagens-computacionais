b02defc897f4e52238eacc9439cc988900c561c8
Ana Paula Sá <114103289+anapsa@users.noreply.github.com>
2024-05-20 20:18:58 -0300

    Update B.hs

--- Lista 1/B.hs
+++ Lista 1/B.hs
@@ -1,12 +1,16 @@
+--Máquina de Somar
 maquinaSomar :: [Int] -> [Int]
-maquinaSomar result = soma result 0 True
+maquinaSomar result = soma result 0 True True
     where
-        soma [] somado _ = [somado] 
-        soma (x:xs) somado firstZero
-            | x == 0 && somado == 0 && firstZero == True = soma xs 0 False
-            | x == 0 && somado == 0 && firstZero == False = []
-            | x == 0 = somado : soma xs 0 False
-            | otherwise = soma xs (somado + x) False
+        soma [] somado firstZero realizouSoma
+            | somado == 0 && realizouSoma == True = []
+            | otherwise = [somado]
+        soma (x:xs) somado firstZero realizouSoma
+            | x == 0 && xs == [] && somado == 0 && realizouSoma == True = []
+            | x == 0 && realizouSoma == False = somado : soma xs 0 False True
+            | x == 0 && somado == 0 && firstZero == True = soma xs 0 False realizouSoma --achou o primeiro zero
+            | x == 0 && somado == 0 && firstZero == False = [] --achou o segundo zero
+            | otherwise = soma xs (somado + x) True False
 
 main = do
        lista <- getLine
